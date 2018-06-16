b22978d9e4100f67c579336bce5271365da75aa9
Dheerajchandra <dheeraj.palusa@devrabbit.com>
2018-06-17 02:52:28 +0530

    olx

--- app/common/home.component.js
+++ app/common/home.component.js
@@ -2,66 +2,86 @@ import React, { Component } from 'react';
 import Modal from './modal.js'
 //import $ from 'jquery'
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 class Home extends Component {
-  constructor(props)
-  {
+  constructor(props) {
     console.log('constructor')
     super(props)
-    this.state ={
-      testData:[],
-      json:{userId:2},
-      parent:'dheeraj',
-      showModal:false,
-      value:"test",
-      value1:"test2"
+    this.state = {
+      testData: [],
+      json: { userId: 2 },
+      parent: 'dheeraj',
+      showModal: false,
+      value: "test",
+      value1: "test2",
+      selected:'',
+      subCategory:'',
+      category:'',
+      olx: [{
+        id: 1, menu: 'apartment',
+        subMenu: [{ id: 1, menu: 'one', name: ['kitchen', '1befroom', '1 hall'] }, { id: 2, menu: 'two', name: ['2 kitchen', '2 bedroom', '2 hall'] }, { id: 3, menu: 'three', name: ['3 kitchen', '3 bedroom', '3 hall'] }]
+      },
+      {
+        id: 2, menu: 'car',
+        subMenu: [{ id: 1, menu: 'svu', name: ['1 baleno', '1 ritz', '1 Swift'] }, { id: 2, menu: 'saden', name: ['2 baleno', '2 ritz', '2 Swift'] }, { id: 3, menu: 'hatchback', name: ['3 baleno', '3 ritz', '3 Swift'] }]
+      },
+      {
+        id: 3, menu: 'electronics',
+        subMenu: [{ id: 1, menu: 'phone', name: ['1 Lg', '1 samsung', '1 Sony'] }, { id: 2, menu: 'AC', name: ['2 Lg', '2 samsung', '2 Sony'] }, { id: 3, menu: 'Alexa', name: ['3 Lg', '3 samsung', '3 Sony'] }]
+      }]
     }
   }
 
-   async componentWillMount(){
+  setChecked(object) {
+
+    this.setState(object);
+}
+
+  async componentWillMount() {
     // console.log('componentWillMount')
-   }
-    // this.setState({testData:this.state.json})
-    //  await fetch('https://jsonplaceholder.typicode.com/posts/1',{ 
-    //   method: 'GET'
-    // })
-    //  .then(response => response.json())
-    // // .then(json => this.setState({testData:json}))
-    // .then(json => console.log('will'))
-   
+  }
+  // this.setState({testData:this.state.json})
+  //  await fetch('https://jsonplaceholder.typicode.com/posts/1',{ 
+  //   method: 'GET'
+  // })
+  //  .then(response => response.json())
+  // // .then(json => this.setState({testData:json}))
+  // .then(json => console.log('will'))
 
-   async componentDidMount(){
-   // console.log('componentDidMount')
-    await fetch('https://jsonplaceholder.typicode.com/posts/1',{ 
-     method: 'GET'
-   })
-    .then(response => response.json())
-    .then(json => this.setState({testData:json}))
+
+  async componentDidMount() {
+    // console.log('componentDidMount')
+    await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(json => this.setState({ testData: json }))
   }
 
 
-   changeParent(name){
-    this.setState({parent:name})
-   }
-  modal(){
-   // console.log('fdf')
-    
-   // $("p").css("background-color", "yellow");
-  //  $("#myModal").modal('show');
-  this.setState({testData:this.state.json,showModal:true})
+  changeParent(name) {
+    this.setState({ parent: name })
+  }
+  modal() {
+    // console.log('fdf')
+
+    // $("p").css("background-color", "yellow");
+    //  $("#myModal").modal('show');
+    this.setState({ testData: this.state.json, showModal: true })
   }
 
-    render(){
-      console.log('==render===')
-     
-      return (<div><p>{this.state.parent}</p><br/>
-       <div>
-       <button type="button" disabled={this.state.email} className="btn btn-info btn-lg" data-toggle='modal' data-target="#myModal" onClick={this.modal.bind(this)} >open modal</button>
-       <br/><input type="text" value={this.state.value != "test" ? this.state.value:this.state.value1}></input>
-       </div>
-      <Modal value={this.state.testData} change={this.changeParent.bind(this)}/>
-       </div>)
-    }
+  render() {
+    console.log('==render===',this.state)
+
+    return (<div><p>{this.state.parent}</p><br />
+      <div>
+        <button type="button" disabled={this.state.email} className="btn btn-info btn-lg" data-toggle='modal' data-target="#myModal" onClick={this.modal.bind(this)} >open modal</button>
+        <br /><input type="text" value={this.state.value != "test" ? this.state.value : this.state.value1}></input>
+      {this.state.category == ""?'':(this.state.category + " => " +this.state.subCategory + " => " +this.state.selected)  }
+      </div>
+      <Modal value={this.state.olx} change={this.changeParent.bind(this)} setChecked={this.setChecked.bind(this)}/>
+    </div>)
+  }
 }
 
 export default Home
--- app/common/modal.js
+++ app/common/modal.js
@@ -1,78 +1,136 @@
 /* Importing the node modules, child components, services and controllers used 
    inside BookNow component */
-   import React from 'react';
-  
-   
-   import {Link} from 'react-router-dom';
-   import DatePicker from 'react-datepicker'
-   import moment from 'moment'
-  
+import React from 'react';
 
-   /* BookNow Component initialization */
-   class BookNow extends React.Component {
-       /* Initializing objects of its BookNow class */
-       constructor(props) {
-        console.log('modalconstructor')
-           super(props)
-           console.log('==props==',props)
-            this.state={
-                
-               
+
+import { Link } from 'react-router-dom';
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+
+
+/* BookNow Component initialization */
+class BookNow extends React.Component {
+  /* Initializing objects of its BookNow class */
+  constructor(props) {
+    console.log('modalconstructor')
+    super(props)
+    console.log('==props==', props)
+    this.state = {
+      showMenus: false,
+      type: 0,
+      showSubMenus: false,
+      subMenuType: 0,
+      subCatName:'',
+      subMenuName:''
+
+    }
+  }
+
+  async componentWillMount() {
+    console.log('modalcomponentWillMount')
+  }
+
+  async componentDidMount() {
+    console.log('modalcomponentDidMount')
+  }
+
+  componentWillReceiveProps(nextProps, prevProps) {
+
+    console.log('componentWillReceiveProps', nextProps, prevProps)
+    this.setState({ showMenus: false, showSubMenus: false })
+  }
+
+
+  parentChange() {
+
+    //  this.props.change('teja')
+
+    setTimeout(async function () {
+      jQuery(function ($) {
+        $('#myModal').modal('hide');
+      });
+    }, 10000);
+  }
+
+  showMore(index, row) {
+    this.setState({ showMenus: true, type: index, showSubMenus: false,subCatName:row.menu })
+  }
+  showSubCategories(index,row) {
+    this.setState({ showMenus: true, subMenuType: index, showSubMenus: true, subMenuName:row.menu })
+  }
+
+  selected(row) {
+    var object = {};
+    object['selected'] = row;
+    object['category'] = this.state.subCatName;
+    object['subCategory'] = this.state.subMenuName;
+    this.setState(object);
+    this
+        .props
+        .setChecked(object)
+ }
+
+  /* It is invoked to return html content */
+  render() {
+    console.log('yyy', this.state)
+    return (
+      <div id="myModal" className="modal fade" role="dialog" data-backdrop="static">
+        <div className="modal-dialog">
+
+
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <h4 className="modal-title">Modal Header</h4>
+            </div>
+            {this.state.showMenus ? '' :
+              <div className="modal-body">
+                <div className="row">
+                  {this.props.value.map((row, index) => {
+                    return <div className="col-sm-6" key={index} ><p onClick={this.showMore.bind(this, index, row)}>{row.menu}</p></div>
+                  })}
+                </div>
+              </div>
             }
-       }
+            <div className="modal-body">
+              <div className="row">
+                {this.state.showMenus ?
 
-       async componentWillMount(){
-        console.log('modalcomponentWillMount')
-       }
+                  <div>
+                    <div className="col-sm-4">
+                      <h4>Categories</h4>
+                      {this.props.value.map((row, index) => {
+                        return <p key={index} onClick={this.showMore.bind(this, index, row)}>{row.menu}</p>
+                      })}
+                    </div>
 
-       async componentDidMount(){
-        console.log('modalcomponentDidMount')
-       }
+                    <div className="col-sm-4">
+                      <h4>{this.state.subCatName}</h4>
+                      {this.props.value[this.state.type].subMenu.map((row, index) => {
+                        return <p key={index} onClick={this.showSubCategories.bind(this, index,row)}>{row.menu}</p>
+                      })}
+                    </div>
 
-       componentWillReceiveProps(nextProps,prevProps){ 
-       
-        console.log('componentWillReceiveProps',nextProps,prevProps)
-      
-        }
+                  </div>
+                  : ''}
 
+                {this.state.showSubMenus ? <div className="col-sm-4">
+                  <h4>{this.state.subMenuName}</h4>
+                  {this.props.value[this.state.type].subMenu[this.state.subMenuType].name.map((row, index) => {
+                    return <p key={index} onClick={this.selected.bind(this,row)} >{row}</p>
+                  })}
+                </div> : ''}
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" onClick={this.parentChange.bind(this)} >Close</button>
+            </div>
+          </div>
 
-        parentChange(){
-          
-        //  this.props.change('teja')
-
-        setTimeout(async function() {
-          jQuery(function($) {
-              $('#myModal').modal('hide');
-          });
-   }, 10000);
-        }
-
-      
-       /* It is invoked to return html content */
-       render() {
-           console.log('yyy')
-           return (
-            <div id="myModal" className="modal fade" role="dialog" data-backdrop="static">
-  <div className="modal-dialog">
-
-   
-    <div className="modal-content">
-      <div className="modal-header">
-        <button type="button" className="close" data-dismiss="modal">&times;</button>
-        <h4 className="modal-title">Modal Header</h4>
+        </div>
       </div>
-      <div className="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-default" onClick={this.parentChange.bind(this)} >Close</button>
-      </div>
-    </div>
 
-  </div>
-</div>
-   
-           );
-       }
-   }
-   export default BookNow;
+    );
+  }
+}
+export default BookNow;
